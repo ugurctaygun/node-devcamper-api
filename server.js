00133cb34da618d31f0158d726cd79ec9f2734ff
ugurctaygun <ugurctaygun@gmail.com>
2020-04-20 21:31:20 +0300

    mongoose,mongoDB

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
+
+//Connect to Database
+connectDB();
 
 const app = express();
 
